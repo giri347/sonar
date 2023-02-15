04a6a9ac37a8e4ad9393b580109589b2a80b266b
giri347 <92531117+giri347@users.noreply.github.com>
2023-02-15 15:23:56 +0530

    Create linux-webapp.tf

---
+++ linux-webapp.tf
@@ -0,0 +1,18 @@
+resource "azurerm_service_plan" "webplan" {
+  name                = var.webplanname
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+  tags                = var.tags
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = var.webappname
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.webplan.id
+  tags                = var.tags
+
+  site_config {}
+}
