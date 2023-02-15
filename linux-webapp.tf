12fca43e9a723688a272b3372932956aef276500
Giribabu Chitteni <Giribabu.Chitteni@einfochips.com>
2023-02-15 15:30:48 +0530

    modified

--- linux-webapp.tf
+++ linux-webapp.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "webplan" {
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
   tags                = var.tags
 }
 
